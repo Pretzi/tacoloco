01c337cbcf910b704a7ae9a97fa33cda36799e7b
Pretzi <m_pretelin@hotmail.com>
2026-03-03 16:58:42 -0600

    fixes

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [],
   },
-  // Run next-intl (and its runtime) in Node instead of bundling (avoids __dirname in ESM bundle on Vercel)
-  serverExternalPackages: ["next-intl", "use-intl"],
 };
 
 export default withNextIntl(nextConfig);
